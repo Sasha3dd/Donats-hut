83528c61c4fc666912207ba6bb967f0e746e9bc3
Sasha3dd <156731106+Sasha3dd@users.noreply.github.com>
2024-01-16 13:21:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // scripts.js
 
-function orderDonut() {
-    alert("Дякуємо за ваше замовлення! Очікуйте на свої смачні пончики!");
+function goToPurchase(donutType) {
+    // Додати код для переходу до сторінки purchase.html з параметром donutType
+    window.location.href = 'purchase.html?type=' + donutType;
 }
 
-function buyDonut() {
-    alert("Дякуємо за вашу покупку! Насолоджуйтесь своїми смачними пончиками!");
+function searchDonuts() {
+    // Додати код для пошуку пончиків
+    var searchTerm = document.getElementById('searchInput').value; // Припустимо, що у вас є поле вводу з id="searchInput"
+    // Далі ви можете використовувати searchTerm для реалізації логіки пошуку
 }
